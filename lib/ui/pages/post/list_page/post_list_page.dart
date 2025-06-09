77fa694f2e1ce3a8d66db88208bb0351b5ddde83
JM <lsaoe157@naver.com>
2025-06-09 11:55:37 +0900

    게시글 목록보기 페이징 직전까지 완료

--- lib/ui/pages/post/list_page/post_list_page.dart
+++ lib/ui/pages/post/list_page/post_list_page.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog/data/gvm/session_gvm.dart';
 import 'package:flutter_blog/ui/pages/post/list_page/wiegets/post_list_body.dart';
 import 'package:flutter_blog/ui/widgets/custom_navigator.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final scaffoldKey = GlobalKey<ScaffoldState>();
 
-class PostListPage extends ConsumerWidget {
-  final refreshKey = GlobalKey<RefreshIndicatorState>();
-
+class PostListPage extends StatefulWidget {
   PostListPage();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    SessionModel model = ref.read(sessionProvider);
+  State<PostListPage> createState() => _PostListPageState();
+}
 
+class _PostListPageState extends State<PostListPage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
       drawer: CustomNavigation(scaffoldKey),
       appBar: AppBar(
         // 로그인을 수행해야 저 화면이 넘어오기 때문에 user는 절대 null이 될 수 없음 : ! 붙이기
-        title: Text("Blog ${model.isLogin} ${model.user!.username}"),
+        title: Text("Blog"),
       ),
-      body: RefreshIndicator(
-        key: refreshKey,
-        onRefresh: () async {},
-        child: PostListBody(),
-      ),
+      body: PostListBody(),
     );
   }
 }
