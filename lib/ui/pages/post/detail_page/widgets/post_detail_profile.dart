fae9d045fa167683484b5ea97dd7439776485eef
JM <lsaoe157@naver.com>
2025-06-09 17:52:03 +0900

    삭제 완료

--- lib/ui/pages/post/detail_page/widgets/post_detail_profile.dart
+++ lib/ui/pages/post/detail_page/widgets/post_detail_profile.dart
@@ -1,28 +1,37 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/_core/constants/size.dart';
+import 'package:flutter_blog/_core/utils/my_http.dart';
+import 'package:flutter_blog/data/model/post.dart';
+import 'package:intl/intl.dart';
 
 class PostDetailProfile extends StatelessWidget {
-
-  const PostDetailProfile({Key? key}) : super(key: key);
+  Post post;
+  PostDetailProfile(this.post);
 
   @override
   Widget build(BuildContext context) {
+    final formattedDate = DateFormat("MMM d").format(post.createdAt); // 예: May 25
+
     return ListTile(
-        title: Text("ssar"),
-        leading: ClipRRect(
-          borderRadius: BorderRadius.circular(50),
-          child: Image.asset('assets/default_profile.png'),
+        title: Text("${post.user.username}"),
+        leading: ClipOval(
+          child: CachedNetworkImage(
+            imageUrl: "${baseUrl}${post.user.imgUrl}",
+            fit: BoxFit.cover,
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
         ),
         subtitle: Row(
           children: [
-            Text("ssar@nate.com"),
+            Text("ssar@nate.com"), // api 서버 개발자한테 요청
             const SizedBox(width: mediumGap),
             const Text("·"),
             const SizedBox(width: mediumGap),
             const Text("Written on "),
-            Text("May 25"),
+            Text(formattedDate),
           ],
-        )
-    );
+        ));
   }
 }
